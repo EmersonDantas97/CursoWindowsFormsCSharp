1624d4eb94d27dd5fda1013a707d0c5e89e0c7f1
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2023-01-23 19:08:17 -0300

    alteracao 23/01/23 19:08

---
+++ CursoWindowsForms/Frm_Principal_Menu_UC.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_Principal_Menu_UC : Form
+    {
+        public Frm_Principal_Menu_UC()
+        {
+            InitializeComponent();
+        }
+        private void demonstraçãoKeyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_DemonstracaoKey f = new Frm_DemonstracaoKey();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void helloWorldToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_HelloWorld_UC U = new Frm_HelloWorld_UC();
+            TabPage TB = new TabPage();
+            TB.Name = "Hello World";
+            TB.Text = "Hello World";
+            TB.Controls.Add(U);
+            Tbc_Aplicacoes.TabPages.Add(TB);
+        }
+
+        private void máscaraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_Mascara f = new Frm_Mascara();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void validaCPFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF f = new Frm_ValidaCPF();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void validaCPF2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaCPF2 f = new Frm_ValidaCPF2();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void validaSenhaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Frm_ValidaSenha f = new Frm_ValidaSenha();
+            f.MdiParent = this;
+            f.Show();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cascataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+        }
+
+        private void horizontalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+        }
+
+        private void verticalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileVertical);
+        }
+    }
+}
