79ec63f334888f485bbeaeedce7b5f567ec12b8c
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2023-01-23 13:58:45 -0300

    alteracao 23/01/23 13:58

---
+++ CursoWindowsForms/Formulários Curso 1/Frm_HelloWord.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_HelloWord : Form
+    {
+        public Frm_HelloWord()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Sair_Click(object sender, EventArgs e)
+        {
+            this.Close(); // Fechando somente o Form.
+            //Application.Exit(); // Aplicando função para fechar a aplicação.
+        }
+
+        private void btn_ModificaLabel_Click(object sender, EventArgs e)
+        {
+            lbl_Titulo.Text = txt_ConteudoLabel.Text;
+        }
+
+        private void Frm_HelloWord_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
