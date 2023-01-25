cffb68a5f43a2be59e4af34a4e8379be81609ffa
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2023-01-25 14:03:13 -0300

    alteracao 25/01/23 14:03

--- CursoWindowsForms/Properties/Resources.Designer.cs
+++ CursoWindowsForms/Properties/Resources.Designer.cs
@@ -189,5 +189,25 @@ namespace CursoWindowsForms.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap interrogation_mark {
+            get {
+                object obj = ResourceManager.GetObject("interrogation_mark", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Consulta um recurso localizado do tipo System.Drawing.Icon semelhante a (Ícone).
+        /// </summary>
+        internal static System.Drawing.Icon interrogation_mark1 {
+            get {
+                object obj = ResourceManager.GetObject("interrogation_mark1", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
