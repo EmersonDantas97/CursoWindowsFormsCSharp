55b44d0071cbaf679753089bacacfd93f3fa18cc
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2023-01-27 13:17:07 -0300

    alteracao 27/01/23 13:17

---
+++ CursoWindowsForms/Frm_MenuFlutuante.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CursoWindowsForms
+{
+    public partial class Frm_MenuFlutuante : Form
+    {
+        public Frm_MenuFlutuante()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_MenuFlutuante_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                var PosicaoX = e.X;
+                var PosicaoY = e.Y;
+
+                MessageBox.Show("Você clicou no botão direito! A posicao foi " + PosicaoX + " e " + PosicaoY + ".");
+
+            }
+        }
+    }
+}
