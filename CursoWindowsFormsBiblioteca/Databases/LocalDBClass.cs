1174721366d2bdff1fe976a566d9926e648f6f3c
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2023-02-07 19:56:10 -0300

    alteracao 07/02/23 19:56

---
+++ CursoWindowsFormsBiblioteca/Databases/LocalDBClass.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.SqlClient;
+
+namespace CursoWindowsFormsBiblioteca.Databases
+{
+    public class LocalDBClass
+    {
+        public string stringConn { get; set; }
+        public SqlConnection connDB { get; set; }
+
+        public LocalDBClass()
+        {
+            stringConn = ""; 
+            connDB = new SqlConnection(stringConn); 
+            connDB.Open();
+        }
+
+    }
+}
