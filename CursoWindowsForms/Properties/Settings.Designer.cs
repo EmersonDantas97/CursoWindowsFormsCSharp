cb1780879bae03bda00e4c39a774c4c5787ecf8d
Emerson Dantas <104240809+EmersonDantas97@users.noreply.github.com>
2023-02-15 15:14:46 -0300

    alteracao 15/02/23 15:14

--- CursoWindowsForms/Properties/Settings.Designer.cs
+++ CursoWindowsForms/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace CursoWindowsForms.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.6.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -32,6 +32,17 @@ namespace CursoWindowsForms.Properties {
         public string FicharioConnectionString {
             get {
                 return ((string)(this["FicharioConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-7TLUK34;Initial Catalog=ByteBank;Persist Security Info=True;U" +
+            "ser ID=sa;Password=bispo1997")]
+        public string ByteBankConnectionString {
+            get {
+                return ((string)(this["ByteBankConnectionString"]));
             }
         }
     }
